18ca121c3cfa6bfd1a5d9d646a6590dd3003468b
sljohnson32 <sljohnson32@gmail.com>
2018-02-17 11:48:32 -0700

    Add new card component and history dropdown

--- client/src/components/TapCard/index.js
+++ client/src/components/TapCard/index.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
 import PopUp from '../PopUp';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
 
-class TapCard extends Component {
+export default class TapCard extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+  }
+
+  handleExpandChange = (expanded) => {
+    this.setState({expanded: expanded});
+  };
+
+  toggleExpanded = () => {
+    this.setState({expanded: !this.state.expanded});
+  };
 
   render() {
+
     let { id, name } = this.props;
 
     return (
-      <div className="tap-card">
-        <section className='tap-header'>
-          <h3>{name}</h3>
+      <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
+        <CardTitle
+          title={ name }
+        >
           <PopUp />
-        </section>
-        <section className="tap-info">
+        </CardTitle>
+        <CardText>
           <h4>Current Beer</h4>
           <p>Beer Type</p>
           <h4>Brewer</h4>
-        </section>
-      </div>
+        </CardText>
+        <CardTitle
+          title="Tap History"
+          expandable={true}
+        />
+        <CardText expandable={true}>
+          This is a table of all previous beers!
+        </CardText>
+        <CardActions>
+          <FlatButton label={ this.state.expanded ? "Close History" : "See History" } onClick={this.toggleExpanded} />
+        </CardActions>
+      </Card>
     );
   }
 }
 
-export default TapCard;
+
+// class TapCard extends Component {
+//
+//   render() {
+//     let { id, name } = this.props;
+//
+//     return (
+//       <div className="tap-card">
+//         <section className='tap-header'>
+//           <h3>{name}</h3>
+//           <PopUp />
+//         </section>
+//         <section className="tap-info">
+//           <h4>Current Beer</h4>
+//           <p>Beer Type</p>
+//           <h4>Brewer</h4>
+//         </section>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default TapCard;
